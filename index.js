b89b0874aeff73f7727389822abb01de720eea8d
Braden M. Kelley <redbmk@gmail.com>
2019-05-17 17:08:05 -0700

    implement add, edit, and delete functionality

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, AuthenticationError, gql } = require('apollo-server');
+const uuid = require('uuid/v4');
+
 const { getToken, getUserIdFromToken, getUser } = require('./auth');
 
 const typeDefs = gql`
   type Quote {
+    id: ID!
     phrase: String!
     quotee: String
   }
@@ -14,37 +17,66 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(username: String!, password: String!): Authentication!
+    login(username: String!, password: String!): Authentication
+    addQuote(phrase: String!, quotee: String): Quote
+    editQuote(id: ID!, phrase: String, quotee: String): Quote
+    deleteQuote(id: ID!): DeleteResponse
   }
 
   type Authentication {
     token: String!
   }
+
+  type DeleteResponse {
+    ok: Boolean!
+  }
 `;
 
-const quotes = [
-  {
-    phrase: "I'm a leaf on the wind. Watch how I soar.",
-    quotee: "Wash",
-  },
-  {
-    phrase: "We're all stories in the end. Just make it a good one, eh?",
-    quotee: "The Doctor",
-  },
-  {
-    phrase: "Woah",
-    quotee: "Neo",
-  },
-];
+const quotes = {};
+const addQuote = quote => {
+  const id = uuid();
+  return quotes[id] = { ...quote, id };
+};
+
+addQuote({ phrase: "I'm a leaf on the wind. Watch how I soar.", quotee: "Wash" });
+addQuote({ phrase: "We're all stories in the end.", quotee: "The Doctor" });
+addQuote({ phrase: "Woah!", quotee: "Neo" });
 
 const resolvers = {
   Query: {
-    quotes: () => quotes,
+    quotes: () => Object.values(quotes),
   },
   Mutation: {
     login: async (parent, { username, password }) => ({
       token: await getToken({ username, password }),
     }),
+    addQuote: async (parent, quote, context) => {
+      if (!context.user) throw new AuthenticationError("You must be logged in to perform this action");
+
+      return addQuote(quote);
+    },
+    editQuote: async (parent, { id, ...quote }, context) => {
+      if (!context.user) throw new AuthenticationError("You must be logged in to perform this action");
+
+      if (!quotes[id]) {
+        throw new Error("Quote doesn't exist");
+      }
+
+      quotes[id] = {
+        ...quotes[id],
+        ...quote,
+      };
+
+      return quotes[id];
+    },
+    deleteQuote: async (parent, { id }, context) => {
+      if (!context.user) throw new AuthenticationError("You must be logged in to perform this action");
+
+      const ok = Boolean(quotes[id]);
+      delete quotes[id];
+
+      return { ok };
+    },
   },
 };
 
